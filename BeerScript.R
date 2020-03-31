9870647c55cf6b8a1930b9c55b3d722abf98c82a
Ischi94 <gmathes94@gmx.de>
2020-03-31 12:15:07 +0200

    Tibble with per capita consumption added

--- BeerScript.R
+++ BeerScript.R
@@ -25,10 +25,22 @@ brewer_size %>%
   mutate(prop_n = n/ sum(n)) %>% 
   ggplot() +
 #  geom_line(aes(year, prop_n, colour = size))
-#  geom_area(aes(year, n, fill = size))
-  geom_col(aes(year, prop_n, fill = size)) +
-  scale_fill_manual(values = c("red", "yellow", "purple")) +
+#  geom_area(aes(year, prop_n, fill = size)) +
+  geom_col(aes(year, prop_n, fill = size), width = 1) +
+  scale_fill_manual(values = c("#ad8599", "#e09952", "#75abbd")) +
   theme_minimal()
 
+# add consumption per capita/ 
+# source: https://eu.usatoday.com/story/money/2019/09/14/how-much-beer-did-the-average-person-drink-in-every-state/40109241/
+
+tibble(states = unique(beer_states$state)[1:51], 
+       per_capita = c(26.9, 28.7, 23.4, 26.8, 25.1, 28.8, 20.0,
+                      NA, 28.4, 26.4, 23.8, 30.6, 31.2, 26.4,
+                      27.7, 23.1, 25.8, 22.9, 28.0, 22.9, 19.6,
+                      33.8, 24.7, 28.5, 27.2, 30.4, 40.8, 25.8,
+                      38.2, 32.5, 39.8, 20.1, 28.8, 33.2, 21.5,
+                      26.9, 24.6, 29.8, 25.9, 23.2, 30.6, 38.2,
+                      24.5, 30.9, 18.6, 24.5, 33.3, 24.3, 33.6,
+                      27.3, 30.1))
 
 
